9b5f35103ffbe2cc89b1bfa4a2fad05ba621df5c
Yongwoo, Lee <leeyw910205@gmail.com>
2020-11-18 03:03:16 +0900

    Initial commit

---
+++ src/main/java/com/pay/money/throwing/ThrowingApplication.java
@@ -0,0 +1,13 @@
+package com.pay.money.throwing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrowingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThrowingApplication.class, args);
+    }
+
+}
