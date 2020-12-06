a0c5c4a5a2f7d2cc9ea17f4b98aa58a3a79598bf
Yongwoo, Lee <leeyw910205@gmail.com>
2020-12-06 22:44:00 +0900

    fix: redis 트랜잭션 관리 설정 추가

--- src/main/java/com/pay/money/throwing/config/RedisConfig.java
+++ src/main/java/com/pay/money/throwing/config/RedisConfig.java
@@ -28,6 +28,7 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(){
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setEnableTransactionSupport(true);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         return redisTemplate;
     }
